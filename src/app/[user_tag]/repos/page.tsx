627d86733122801613d2514ec50445f536af9e6e
kmpizmad <nagy.viktor392@gmail.com>
2025-03-05 10:37:18 +0100

    build: fix build issue

--- src/app/[user_tag]/repos/page.tsx
+++ src/app/[user_tag]/repos/page.tsx
@@ -1,15 +1,19 @@
 import BackButton from '@/components/common/BackButton';
 import RepositoryList from '@/views/RepositoryList';
 
-export default function UserRepositories({ params }: { params: { user_tag: string } }) {
+type Params = { user_tag: string };
+
+export default async function UserRepositories({ params }: { params: Promise<Params> }) {
+  const { user_tag } = await params;
+
   return (
     <div>
       <div className="flex items-center justify-between">
-        <h1 className="font-bold text-body">{params.user_tag}&apos;s Repositories</h1>
+        <h1 className="font-bold text-body">{user_tag}&apos;s Repositories</h1>
         <BackButton />
       </div>
       <hr className="mt-4 mb-6 bg-primary-border" />
-      <RepositoryList tag={params.user_tag} />
+      <RepositoryList tag={user_tag} />
     </div>
   );
 }
