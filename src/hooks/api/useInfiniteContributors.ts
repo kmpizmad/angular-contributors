ce420995c925ce576f20f9bdc49b091714b5fd6a
kmpizmad <nagy.viktor392@gmail.com>
2025-03-05 01:25:08 +0100

    refactor: moved utility functions to utils.ts

--- src/hooks/api/useInfiniteContributors.ts
+++ src/hooks/api/useInfiniteContributors.ts
@@ -1,13 +1,16 @@
 'use client';
 
 import { IContributor } from '@/interfaces/entities/IContributor';
-import { httpClient } from '@/lib/http';
+import { fetchPage } from '@/lib/utils';
 import { useInfiniteQuery } from '@tanstack/react-query';
 
 export default function useInfiniteContributors() {
   return useInfiniteQuery({
     queryKey: ['contributors'],
-    queryFn: ({ pageParam }) => fetchPage(pageParam),
+    queryFn: ({ pageParam }) =>
+      fetchPage<IContributor[]>(
+        `https://api.github.com/repos/angular/angular/contributors?page=${pageParam}&per_page=15`
+      ),
     initialPageParam: 1,
     getNextPageParam: page => page.nextPage,
     getPreviousPageParam: page => page.prevPage,
@@ -15,45 +18,4 @@ export default function useInfiniteContributors() {
     retryDelay: 3000,
     retryOnMount: false,
   });
-}
-
-async function fetchPage(page: number) {
-  const response = await httpClient.get<IContributor[]>(
-    `https://api.github.com/repos/angular/angular/contributors?page=${page}&per_page=15`,
-    {
-      headers: {
-        Accept: 'application/vnd.github.v3+json',
-      },
-    }
-  );
-
-  const paginationLinks = parseLinkHeader(response.headers.link);
-
-  return {
-    data: response.data,
-    nextPage: paginationLinks.next ?? null,
-    prevPage: paginationLinks.prev ?? null,
-    firstPage: paginationLinks.first ?? null,
-    lastPage: paginationLinks.last ?? null,
-  };
-}
-
-function parseLinkHeader(linkHeader: string) {
-  if (!linkHeader) return {};
-
-  const links: Record<string, number> = {};
-  const linkParts = linkHeader.split(',');
-
-  linkParts.forEach(part => {
-    const match = part.match(/<([^>]+)>;\s*rel="([^"]+)"/);
-    if (match) {
-      const url = match[1];
-      const rel = match[2];
-      const urlObj = new URL(url);
-      const page = Number(urlObj.searchParams.get('page'));
-      links[rel] = page;
-    }
-  });
-
-  return links;
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,45 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { httpClient } from './http';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function fetchPage<T>(url: string) {
+  const response = await httpClient.get<T>(url, {
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+    },
+  });
+
+  const paginationLinks = parseLinkHeader(response.headers.link);
+
+  return {
+    data: response.data,
+    nextPage: paginationLinks.next ?? null,
+    prevPage: paginationLinks.prev ?? null,
+    firstPage: paginationLinks.first ?? null,
+    lastPage: paginationLinks.last ?? null,
+  };
+}
+
+export function parseLinkHeader(linkHeader: string) {
+  if (!linkHeader) return {};
+
+  const links: Record<string, number> = {};
+  const linkParts = linkHeader.split(',');
+
+  linkParts.forEach(part => {
+    const match = part.match(/<([^>]+)>;\s*rel="([^"]+)"/);
+    if (match) {
+      const url = match[1];
+      const rel = match[2];
+      const urlObj = new URL(url);
+      const page = Number(urlObj.searchParams.get('page'));
+      links[rel] = page;
+    }
+  });
+
+  return links;
 }
