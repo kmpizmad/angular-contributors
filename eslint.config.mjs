0e9a05d72e21dcf4a16d208c3ddaee4959d62812
kmpizmad <nagy.viktor392@gmail.com>
2025-03-05 10:51:47 +0100

    chore: optimize and fix linter settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,10 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
-import pluginQuery from '@tanstack/eslint-plugin-query';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPrettier from 'eslint-config-prettier';
+import tanstackPlugin from '@tanstack/eslint-plugin-query';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,9 +14,20 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:@typescript-eslint/recommended', 'prettier'),
-  ...pluginQuery.configs['flat/recommended'],
-  ...compat.config({
+  {
+    ignores: ['node_modules/', '.next/', 'coverage/', '**/*.test.js', '**/*.test.ts'],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: '@typescript-eslint/parser',
+    },
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...tseslint.config(eslint.configs.recommended, tseslint.configs.recommended),
+  ...tanstackPlugin.configs['flat/recommended'],
+  {
     rules: {
       'newline-before-return': 'error',
       'lines-around-comment': [
@@ -38,7 +52,8 @@ const eslintConfig = [
       '@typescript-eslint/no-unsafe-function-type': ['error'],
       '@typescript-eslint/no-wrapper-object-types': ['error'],
     },
-  }),
+  },
+  eslintPrettier,
 ];
 
 export default eslintConfig;
